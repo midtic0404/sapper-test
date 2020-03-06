d206473eb37e5f5030c8222a988a54c41667e241
cchechang <cchechang@mastercontrol.com>
2020-03-06 13:42:39 -0700

    test

---
+++ src/routes/blog/index.svelte
@@ -0,0 +1,33 @@
+<script>
+	export let posts = ["Hello!", "World!"];
+	let text = '';
+
+	function addText() {
+		posts = [...posts, text];
+	}
+</script>
+
+<style>
+	ul {
+		margin: 0 0 1em 0;
+		line-height: 1.5;
+	}
+</style>
+
+<svelte:head>
+	<title>Blog</title>
+</svelte:head>
+
+<h1>Recent posts</h1>
+<input type="text" bind:value={text} />
+<button on:click={addText}>Add Post</button>
+
+<ul>
+	{#each posts as post}
+		<!-- we're using the non-standard `rel=prefetch` attribute to
+				tell Sapper to load the data for the page as soon as
+				the user hovers over the link or taps it, instead of
+				waiting for the 'click' event -->
+		<li><a rel='prefetch' href='blog/{post}'>{post}</a></li>
+	{/each}
+</ul>
